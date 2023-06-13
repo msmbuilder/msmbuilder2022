d49ed2ab8cd6860d4f0d8b7f8700806fa76a7646
bojunliu0818 <bliu293@wisc.edu>
2023-06-13 00:26:51 -0500

    implement BACE (copy from msmbuilder archive)

--- msmbuilder/lumping/__init__.py
+++ msmbuilder/lumping/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import, print_function, division
 
 from .pcca import PCCA
 from .pcca_plus import PCCAPlus
+from .bace import BACE
 
-__all__ = ["PCCA", "PCCAPlus"]
+__all__ = ["PCCA", "PCCAPlus", "BACE"]
