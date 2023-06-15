f611cce6483e8000b8e597d708b4e2c6064c67ff
Sirui Li <sli869@wisc.edu>
2023-06-15 14:05:37 -0500

    implement MVCA (copy from msmbuilder archive)

--- msmbuilder/tests/test_lumping.py
+++ msmbuilder/tests/test_lumping.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import numpy as np
 from sklearn.pipeline import Pipeline
 
-from msmbuilder.lumping import PCCA, PCCAPlus, BACE
+from msmbuilder.lumping import PCCA, PCCAPlus, MVCA, BACE
 from msmbuilder.msm import MarkovStateModel
 
 random = np.random.RandomState(9)
@@ -87,6 +87,21 @@ def test_bace_2():
     assert (np.min(assignments) >= 0)
 
 
+def test_mvca():
+    assignments, ref_macrostate_assignments = _metastable_system()
+    pipeline = Pipeline([
+        ('msm', MarkovStateModel()),
+        ('mvca', MVCA(n_macrostates=2))
+    ])
+    macro_assignments = pipeline.fit_transform(assignments)[0]
+    # we need to consider any permutation of the state labels when we
+    # test for equality. Since it's only a 2-state that's simple using
+    # the logical_not to flip the assignments.
+    opposite = np.logical_not(ref_macrostate_assignments)
+    assert (np.all(macro_assignments == ref_macrostate_assignments) or
+            np.all(macro_assignments == opposite))
+    
+
 def test_from_msm():
     assignments, _ = _metastable_system()
     msm = MarkovStateModel()
@@ -97,6 +112,10 @@ def test_from_msm():
     msm.fit(assignments)
     pccaplus = PCCAPlus.from_msm(msm, 2)
 
+    msm = MarkovStateModel()
+    msm.fit(assignments)
+    mvca = MVCA.from_msm(msm, 2)
+    
     msm = MarkovStateModel()
     msm.fit(assignments)
     bace = BACE.from_msm(msm, 2)
